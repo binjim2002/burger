2f93ab26a17ef8e287e400978cee211c39829d45
binjim2002 <binjim2002@yahoo.fr>
2020-07-14 16:05:57 -0400

    styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,36 +16,23 @@ router.get("/", async function(req, res) {
   var hbsObject = {
     burgers: allBurgers
   };
-  console.log(hbsObject);
+  
   res.render("index", hbsObject);
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", async function(req, res) {
+  const newBurger = await burger.create(req.body.burger_name);
+
+  res.json({id:newBurger.insertId})
+
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", async function(req, res) {
 
-  console.log("condition", condition);
+  await burger.devour(req.params.id);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+  res.status(200).end();
+
 });
 
 // Export routes for server.js to use.
